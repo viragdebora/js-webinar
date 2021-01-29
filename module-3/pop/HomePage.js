dec9e872729647d6b429b887b763ef4a7dffc52c
Virág Debóra <viragdebi97@gmail.com>
2021-01-29 15:49:16 +0100

    module-3 HomePage.js update

--- module-3/pop/HomePage.js
+++ module-3/pop/HomePage.js
@@ -4,7 +4,20 @@
  * for it (test/pop/HomePage.spec.js).
  */
 
- const homePage = new Layout("Home","https://epam.com",{css: 'body'});
+const homePage = new Layout("Home", "https://epam.com", { css: 'body' });
 
- const navbar = new Element("Navbar", {css: 'header'});
- 
+const elementsContainer = new Element("Body", {css: 'body'});
+
+const navbar = new Element("Navbar", { css: 'header' });
+
+const backgroundVideo = new Element("Background Video", { css: 'background-video-ui background-video--narrow' });
+const sliderTitle = new Element("Slider Title", { css: 'header' });
+backgroundVideo.addChildren(sliderTitle);
+
+const footer = new Element("Footer", { css: 'footer' });
+
+elementsContainer.addChildren(navbar);
+elementsContainer.addChildren(backgroundVideo);
+elementsContainer.addChildren(footer);
+homePage.addChildren(elementsContainer);
+
